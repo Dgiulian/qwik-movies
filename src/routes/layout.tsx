e684f6a76bc5edb50b6a086d2968a003e5f6c246
Diego Giuliani <giuliani.diego@gmail.com>
2023-04-15 18:28:19 -0300

    chore: initial commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,8 +1,5 @@
-import { component$, Slot } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
+import { component$, Slot } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
@@ -14,13 +11,11 @@ export default component$(() => {
   return (
     <div class="page">
       <main>
-        <Header />
+        {/* <Header /> */}
         <Slot />
       </main>
       <div class="section dark">
-        <div class="container">
-          <Footer />
-        </div>
+        <div class="container">{/* <Footer /> */}</div>
       </div>
     </div>
   );
